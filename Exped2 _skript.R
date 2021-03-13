02dab4129a1d26e824b34f43f8d9328a0ec8687f
JajjusZiegler <janziegler95@web.de>
2021-03-13 10:16:01 +0100

    Habe den dataframe neu geordnet und damit hoffentlich eine bessere grundlage geschaffen für die auswertung

--- Exped2 _skript.R
+++ Exped2 _skript.R
@@ -1,3 +1,6 @@
-data = dateseries
+
+df = data_exped2_factorized %>% select(-5:-38)
+
+write.csv(df, "C:\\Users\\Janus\\Desktop\\UNI LENC\\WS20_21\\Exped 2\\Exped-fucks\\df_long.csv", row.names=FALSE)
 
 data[data == "9999"] <- NA
