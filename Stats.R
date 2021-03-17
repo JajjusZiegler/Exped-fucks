8af9e1c9f541fd33886e94e686eb11be2d4e0ca2
freddyfriction <frederik-maerker@web.de>
2021-03-17 11:10:43 +0100

    just playing around with git

--- Stats.R
+++ Stats.R
@@ -19,14 +19,14 @@ datNO <- dat%>% slice(34448:52577)
 
 #plot soil temperature: boxplot
 ?boxplot
-boxplot(Soiltemp ~ Sensor, data=dat0C, ylab = 'Temperature (°C)')
-boxplot(Soiltemp ~ Sensor, data=dat6C, ylab = 'Temperature (°C)')
-boxplot(Soiltemp ~ Sensor, data=dat12C, ylab = 'Temperature (°C)')
-boxplot(Soiltemp ~ Sensor, data=datISO, ylab = 'Temperature (°C)')
-boxplot(Soiltemp ~ Sensor, data=datNO, ylab = 'Temperature (°C)')
+boxplot(Soiltemp ~ Sensor, data=dat0C, ylab = 'Temperature (?C)')
+boxplot(Soiltemp ~ Sensor, data=dat6C, ylab = 'Temperature (?C)')
+boxplot(Soiltemp ~ Sensor, data=dat12C, ylab = 'Temperature (?C)')
+boxplot(Soiltemp ~ Sensor, data=datISO, ylab = 'Temperature (?C)')
+boxplot(Soiltemp ~ Sensor, data=datNO, ylab = 'Temperature (?C)')
 
 #summerize all in one plot 0C, 6C, 12C, Iso, NoISO
-boxplot(Soiltemp~Treatment, data = datsum, ylab = 'Temperature (°C)')
+boxplot(Soiltemp~Treatment, data = datsum, ylab = 'Temperature (?C)')
 
 #signifant difference between groups?
 library(multcomp)
@@ -63,3 +63,22 @@ budnew <- read_excel('bud-burstnew.xlsx')
 budnew[budnew == 9999] <- NA
 plot(budnew$`open buds` ~ budnew$`post treat. Days`, type = 'l')
 
+#with data ordered by column
+budnew <- read_excel('bud-burstnew.xlsx')
+budnew[budnew == 9999] <- NA
+str(budnew)
+budnew$tree <- as.factor(budnew$tree)
+#plot(budnew$`open buds`~budnew$`post treat. Days`, type ='l')
+library(ggplot2)
+qplot(x=`post treat. Days`, y=`open buds`, 
+      data=budnew, 
+      colour=tree, 
+      xlab = 'days post treatment',
+      ylab = 'open buds') +
+  geom_line()
+
+?qplot
+library(gitcreds)
+gitcreds_get()
+library(credentials)
+set_github_pat()
